e4edb2a99366c6b15a043ff43eea385967fb6f76
gputintsev <g.putintsev@qiwi.ru>
2017-08-31 14:37:37 +0300

    fix(gapi): add AudioSource for existing stream

--- interceptor.py
+++ interceptor.py
@@ -3,6 +3,7 @@ import pykka
 import subprocess
 import speech_recognition as sr
 
+
 class InterceptorActor(pykka.ThreadingActor):
     def __init__(self, manager, mic):
         super(InterceptorActor, self).__init__()
@@ -16,14 +17,14 @@ class InterceptorActor(pykka.ThreadingActor):
         args.insert(1, "../kw.waw")
         p = subprocess.Popen(args)
 
-
         p.wait()
 
         try:
 
             r = sr.Recognizer()
-            audio = r.listen(self.mic)
-            print("Google Speech Recognition thinks you said " + r.recognize_google(audio, key = "AIzaSyA97hSk7WuA-w4fCdHAK9h7LWEhiExb7do"))
+            audio = r.listen(MicHack(self.mic))
+            print("Google Speech Recognition thinks you said " + r.recognize_google(audio,
+                                                                                    key="AIzaSyA97hSk7WuA-w4fCdHAK9h7LWEhiExb7do"))
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
@@ -34,3 +35,20 @@ class InterceptorActor(pykka.ThreadingActor):
     def on_receive(self, message):
         if message["command"] == "kw":
             self.on_keyword()
+
+
+class MicHack(sr.Microphone):
+    def __init__(self, mic):
+        self.mic = mic
+        pass
+
+    def __enter__(self):
+        self.stream = self.mic
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.stream.close()
+        finally:
+            self.stream = None
+            self.audio.terminate()
