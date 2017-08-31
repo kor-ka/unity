ecba30e7898fc160920ac7ad15d79684aa268462
gputintsev <g.putintsev@qiwi.ru>
2017-08-31 14:10:15 +0300

    fix(gapi): wrap with try

--- interceptor.py
+++ interceptor.py
@@ -1,3 +1,4 @@
+import logging
 import pykka
 import subprocess
 import speech_recognition as sr
@@ -14,19 +15,22 @@ class InterceptorActor(pykka.ThreadingActor):
         args.insert(1, "../kw.waw")
         p = subprocess.Popen(args)
 
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            audio = r.listen(source)
 
         p.wait()
 
         try:
+
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                audio = r.listen(source)
 
             print("Google Speech Recognition thinks you said " + r.recognize_google(audio, key = "AIzaSyA97hSk7WuA-w4fCdHAK9h7LWEhiExb7do"))
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        except Exception as ex:
+            logging.exception(ex)
 
     def on_receive(self, message):
         if message["command"] == "kw":
