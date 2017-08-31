decbdfa4d6909431afd786e20e7b3c71cf574879
gputintsev <g.putintsev@qiwi.ru>
2017-08-31 14:02:02 +0300

    fix(sphinx): add google recognition

--- interceptor.py
+++ interceptor.py
@@ -1,6 +1,6 @@
 import pykka
 import subprocess
-
+import speech_recognition as sr
 
 class InterceptorActor(pykka.ThreadingActor):
     def __init__(self, manager):
@@ -13,7 +13,20 @@ class InterceptorActor(pykka.ThreadingActor):
         args.insert(0, 'aplay')
         args.insert(1, "../kw.waw")
         p = subprocess.Popen(args)
+
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+
         p.wait()
+
+        try:
+
+            print("Google Speech Recognition thinks you said " + r.recognize_google(audio, key = "AIzaSyA97hSk7WuA-w4fCdHAK9h7LWEhiExb7do"))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
     def on_receive(self, message):
         if message["command"] == "kw":
