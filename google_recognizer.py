42d014e119be7b03692451b51bd38a1218ae92a8
Gleb Putintsev <kor_ka@actor.im>
2017-09-03 19:20:53 +0300

    fix(gapi): yeld mix stream from google ex

--- google_recognizer.py
+++ google_recognizer.py
@@ -1,8 +1,15 @@
 import logging
+
+import pyaudio as pyaudio
 import pykka
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
+from six.moves import queue
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+
+
 
 
 class GoogleRecognizerActor(pykka.ThreadingActor):
@@ -18,7 +25,7 @@ class GoogleRecognizerActor(pykka.ThreadingActor):
 
     def start_recognize(self):
         requests = (types.StreamingRecognizeRequest(audio_content=chunk)
-                    for chunk in StreamIterator(self.mic))
+                    for chunk in MicrophoneStream(self.mic).generator())
 
         config = types.RecognitionConfig(
             encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
@@ -47,9 +54,51 @@ class GoogleRecognizerActor(pykka.ThreadingActor):
 
             # self.interceptor.tell({"command":"talk", ""})
 
-class StreamIterator:
-    def __init__(self, mic, **kwargs):
-        self.mic_stream = mic
+class MicrophoneStream(object):
+    """Opens a recording stream as a generator yielding the audio chunks."""
+    def __init__(self, mic):
+        self._mic = mic
 
-    def __iter__(self):
-        yield self.mic_stream.read(2048, exception_on_overflow=False)
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self):
+        self._audio_stream = self._mic
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        # self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b''.join(data)
