e3c2718117d06da5229b5a62413a9f37b4d91ab9
Gleb Putintsev <kor_ka@actor.im>
2017-09-03 21:21:50 +0300

    fix(gapi): yeld mix stream from google ex

--- lives_speech.py
+++ lives_speech.py
@@ -31,7 +31,6 @@ class LiveSpeech(Pocketsphinx):
             with self.start_utterance():
                 print("sphinx working...")
                 while self.ad.readinto(self.buf) >= 0:
-                    print(".")
                     self.process_raw(self.buf, self.no_search, self.full_utt)
                     if self.keyphrase and self.hyp():
                         with self.end_utterance():
