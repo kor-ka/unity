2e0466bad3d73afdebfc6e2107e58e36f03995ee
Gleb Putintsev <kor_ka@actor.im>
2017-09-03 20:13:48 +0300

    fix(gapi): yeld mix stream from google ex

--- interceptor.py
+++ interceptor.py
@@ -10,10 +10,11 @@ class InterceptorActor(pykka.ThreadingActor):
         super(InterceptorActor, self).__init__()
         self.ls = ls
         self.rec = GoogleRecognizerActor.start(self.actor_ref, self.ls.get_mic())
+        self.kw_detector = SphinxActor.start(self.actor_ref, self.ls)
         self.manager = manager
 
     def on_start(self):
-        self.ls.detect()
+        self.kw_detector.tell({"command": "detect"})
 
     def on_keyword(self):
         print("kw InterceptorActor")
@@ -27,5 +28,19 @@ class InterceptorActor(pykka.ThreadingActor):
 
     def on_receive(self, message):
         if message["command"] == "resume":
+            self.kw_detector.tell({"command": "detect"})
+        if message["command"] == "kw":
+            self.on_keyword()
+
+
+class SphinxActor(pykka.ThreadingActor):
+    def __init__(self, interceptor, ls):
+        super(SphinxActor, self).__init__()
+        self.ls = ls
+        self.interceptor = interceptor
+
+    def on_receive(self, message):
+        if message["command"] == "detect":
             self.ls.detect()
+            self.interceptor.tell({"command": "kw"})
 
