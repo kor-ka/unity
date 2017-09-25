a91118a33a75b7ee5ad9538495a318165522909c
Gleb Putintsev <kor_ka@actor.im>
2017-09-26 00:27:13 +0300

    feat(plurals): implement

--- func_resolver.py
+++ func_resolver.py
@@ -3,9 +3,7 @@ import logging
 from datetime import datetime
 import pykka
 
-import i18n
-
-
+import plurals
 import tts
 from google_recognizer import GoogleRecognizerActor
 
@@ -27,27 +25,24 @@ class FuncResolverActor(pykka.ThreadingActor):
         logging.exception(exception_value)
 
 
-
 class LocalFunctions(pykka.ThreadingActor):
     def __init__(self, interceptor):
         self.interceptor = interceptor
         self.time_strings = ["time", "врем"]
 
-        i18n.set('fallback', 'en')
-
-        i18n.add_translation('hour', {
+        plurals.add('hour', {
             'zero': u'часов',
             'one': u'час',
             'few': u'часа',
             'many': u'часов'
-        }, locale='en')
+        })
 
-        i18n.add_translation('min', {
+        plurals.add('min', {
             'zero': u'минут',
             'one': u'минута',
             'few': u'минуты',
             'many': u'минут'
-        }, locale='en')
+        })
 
         super(LocalFunctions, self).__init__()
 
@@ -59,4 +54,5 @@ class LocalFunctions(pykka.ThreadingActor):
 
     def on_time_ask(self):
         now = datetime.now()
-        tts.say(u" Cейчас {} {} {} {}".format(now.hour, i18n.t('hour', count=now.hour), now.minute, i18n.t('min', count=now.minute)))
+        tts.say(u" Cейчас {} {} {} {}".format(now.hour, plurals.t('hour', count=now.hour), now.minute,
+                                              plurals.t('min', count=now.minute)))
---
+++ plurals.py
@@ -0,0 +1,26 @@
+pl = {}
+
+
+def add(key, plurals):
+    pl.update({key: plurals})
+
+
+def t(key, count):
+    pl.get(key).get(count_pick(count))
+
+
+def count_pick(count):
+    if count == 0:
+        return "zero"
+    count_many = count % 100
+    if count > 100 and (10 < count_many < 15):
+        return "many"
+
+    last_digit = count % 10
+
+    if last_digit == 1:
+        return "one"
+    elif last_digit > 1 and last_digit < 5:
+        return "few"
+    else:
+        return "many"
