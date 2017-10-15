543027827e38b6ab4154d76f7255f031ec130e5b
Gleb Putintsev <kor_ka@actor.im>
2017-10-15 18:53:17 +0300

    fix(install): add locale export

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get install -y python python-dev python-pip build-essential swig git libpulse-dev portaudio19-dev libasound2-dev mpg123
-export LC_ALL=en_US.UTF-8
+sh el.sh
 pip install -U -r requirements.txt
 pip install --upgrade pocketsphinx --no-cache-dir
