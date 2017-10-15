47852230a640581c9264b721fd454c5d57d679ef
Gleb Putintsev <kor_ka@actor.im>
2017-10-15 19:35:01 +0300

    chore(all): move to python3

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install -y python python-dev python-pip build-essential swig git libpulse-dev portaudio19-dev libasound2-dev mpg123 locales
+apt-get install -y python3 python-dev python3-pip build-essential swig git libpulse-dev portaudio19-dev libasound2-dev mpg123 locales
 sh el.sh
-pip install -U -r requirements.txt
-pip install --upgrade pocketsphinx --no-cache-dir
+pip3 install -U -r requirements.txt
+pip3 install --upgrade pocketsphinx --no-cache-dir
