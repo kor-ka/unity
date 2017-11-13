f03a808803201f2c49aa608a76125671ee6f5ff1
kor-ka <badgrib@gmail.com>
2017-11-13 21:38:21 +0300

    wip(sphinx): keep utterance

--- lives_speech.py
+++ lives_speech.py
@@ -10,6 +10,7 @@ from pocketsphinx import *
 RATE = 22000
 CHUNK = 1024  # 100ms
 
+
 class LiveSpeech(Pocketsphinx):
     def __init__(self, mic, **kwargs):
 
@@ -25,27 +26,23 @@ class LiveSpeech(Pocketsphinx):
         self.buf = bytearray(self.buffer_size)
 
         self.ad = mic
+        self.utterance = self.start_utterance()
 
         super(LiveSpeech, self).__init__(**kwargs)
 
     def detect(self):
-            with self.start_utterance():
-                num_chars_printed = 0
-                for buf in self.ad.generator():
-                    volume_norm = audioop.max(buf, 2)
-                    count = "|" * int((int(volume_norm) / 1000))
-                    transcript = "sphinx working {}".format(count)
-                    overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+        num_chars_printed = 0
+        for buf in self.ad.generator():
+            volume_norm = audioop.max(buf, 2)
+            count = "|" * int((int(volume_norm) / 1000))
+            transcript = "sphinx working {}".format(count)
+            overwrite_chars = ' ' * (num_chars_printed - len(transcript))
 
-                    sys.stdout.write(transcript + overwrite_chars + '\r')
-                    sys.stdout.flush()
-                    num_chars_printed = len(transcript)
+            sys.stdout.write(transcript + overwrite_chars + '\r')
+            sys.stdout.flush()
+            num_chars_printed = len(transcript)
 
-                    self.process_raw(buf, self.no_search, self.full_utt)
-                    if self.keyphrase and self.hyp():
-                        with self.end_utterance():
-                            self.ad.pause_clear()
-                            return
-
-
-
+            self.process_raw(buf, self.no_search, self.full_utt)
+            if self.keyphrase and self.hyp():
+                self.ad.pause_clear()
+                return
